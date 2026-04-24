09ccad1e2ce10e1c4b287ddcd33ead3f278c55d5
ulvibrahimov <ulvi11112008@gmail.com>
2026-04-24 15:15:59 +0000

    Task 11: Remove trailing spaces for semistandard compliance

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
-const args = process.argv.slice(2).map(Number);
-if (args.length <= 1) {
+if (process.argv.length <= 3) {
   console.log(0);
 } else {
+  const args = process.argv.slice(2).map(Number);
   const sortedArgs = args.sort((a, b) => b - a);
-  // Using a Set to handle duplicate biggest numbers if necessary, 
-  // though basic sort works for standard cases
   const uniqueSorted = [...new Set(sortedArgs)];
   if (uniqueSorted.length <= 1) {
     console.log(0);
